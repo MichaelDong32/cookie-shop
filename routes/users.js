f2906836a8ab56b1b420305e23b6283898b75951
george francis <georgefrancis105@gmail.com>
2016-07-22 11:41:59 +1200

    Add user and cart page routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET /users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/new', function(req, res) {
+	res.render('newUser', {})
+})
+
+router.get('/:id/cart', function(req, res) {
+	res.render('shoppingCart', {/* the products in the cart */})
+})
 
 module.exports = router;
