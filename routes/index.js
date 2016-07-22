76894ba41d6114051ab79987758710a2dc2d9236
Kristy & Michael <pair+kristy-mj.michaeldong32@devbootcamp.com>
2016-07-22 15:26:47 +1200

    Cleanup for pull

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {});
 });
 
 router.get('/login', function (req, res) {
@@ -13,5 +13,6 @@ router.get('/login', function (req, res) {
 router.get('/complete', function (req, res) {
 	res.render('complete', {})
 })
+
 
 module.exports = router;
